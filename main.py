682b929f0c2faa4aeba37583a72be30b236e60d0
zakirzaf <mohammad.zafar@ada.ac.uk>
2022-11-04 12:43:15 +0000

    Created difference from answer function

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ num = 0
 
 def generate_random_number():
    return random.randint(0, 100)
+
+def difference_from_answer(guess, answer):
+  if guess == answer:
+    return "Correct"
+  elif guess < answer:
+    return "Too low"
+  elif guess > answer:
+    return "Too high"
