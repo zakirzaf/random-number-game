2f4b58cfd1026064d9e74704ed55f30f40d9a560
zakirzaf <mohammad.zafar@ada.ac.uk>
2022-11-04 12:55:12 +0000

    Created make a guess function

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def difference_from_answer(guess, answer):
     return "Too low"
   elif guess > answer:
     return "Too high"
+
+def make_a_guess(answer):
+  user_guess = int(input("Guess a number\n"))
+  return difference_from_answer(user_guess, answer)
